84eb4be14bfd34b93499935b6a4e346513bfd32c
Ameya Barapatre <ameya.barapatre24@ds.sce.edu.in>
2026-04-08 00:11:59 +0530

    Added Java File

---
+++ Gitjenkins.java
@@ -0,0 +1,7 @@
+public class Gitjenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins and GitHub");
+        System.out.println("I am Ameya");
+        System.out.println("My first Java program using Jenkins");
+    }
+}
